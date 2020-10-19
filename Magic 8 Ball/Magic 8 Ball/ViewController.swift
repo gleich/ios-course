2144d5363b6ef921d33bce3864ed5291e21ce8e5
Matthew Gleich <email@mattglei.ch>
2020-10-18 22:15:54 -0400

    ✨ Add extra text switcher

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var buttonText: UIButton!
     
+    var firstRun = true
+    let ballStates = [#imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
     @IBAction func askButtonTapped(_ sender: UIButton) {
-        let states = [#imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
-        image.image = states.randomElement()
+        // Changing image
+        image.image = ballStates.randomElement()
+        
+        // Changing text if first run
+        if firstRun {
+            buttonText.setTitle("Ask Again", for: .normal)
+            firstRun = false
+        }
     }
     
 }
