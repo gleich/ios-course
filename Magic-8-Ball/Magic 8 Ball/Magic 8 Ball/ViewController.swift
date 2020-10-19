1885256d04c5d1a64393c26c73b71d02ad514c39
Matthew Gleich <email@mattglei.ch>
2020-10-18 21:42:44 -0400

    🎉 Initalize Magic 8 Ball app

---
+++ Magic-8-Ball/Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Matthew Gleich on 10/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
