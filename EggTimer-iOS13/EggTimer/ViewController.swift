5dab8809c90c7ea0f6b3a253005b2bf0aff00a61
Matthew Gleich <email@mattglei.ch>
2020-11-08 17:49:01 -0500

    ✨ Add basic countdown

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -2,14 +2,44 @@
 //  ViewController.swift
 //  EggTimer
 //
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Matthew Gleich 2020
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var topTitle: UILabel!
+    
+    let eggTimes : [String: Int] = [
+        "Soft":   1,
+        "Medium": 7,
+        "Hard":   12,
+    ]
+    var secondsRemaining = 0
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let minutes = eggTimes[sender.currentTitle!]!
+        secondsRemaining = minutes * 60
+        
+        timer = Timer.scheduledTimer(
+            timeInterval: 1.0,
+            target: self,
+            selector: #selector(updateTimer),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            topTitle.text = "Done!"
+        }
+    }
+ 
 }
