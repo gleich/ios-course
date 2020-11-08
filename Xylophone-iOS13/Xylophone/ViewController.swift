d751920e43cdaddb2d87aaf80e40232ea9217d9f
Matthew Gleich <email@mattglei.ch>
2020-11-08 14:43:25 -0500

    ✨ Chance opacity of button after press

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
         playSound(soundName: sender.currentTitle!)
     }
     
