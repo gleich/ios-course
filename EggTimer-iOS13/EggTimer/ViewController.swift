64ffb3891598a8090b91d33cfc0c32637abc60c0
Matthew Gleich <email@mattglei.ch>
2020-11-09 12:12:17 -0500

    🎨 Format egg timer with swimat

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var topTitle: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    
-    let eggTimes : [String: Int] = [
-        "Soft":   4,
+
+    let eggTimes: [String: Int] = [
+        "Soft": 4,
         "Medium": 7,
-        "Hard":   12,
+        "Hard": 12,
     ]
     var secondsPassed = 0
     var totalSeconds = 0
     var timer = Timer()
-    
+
     @IBAction func hardnessSelected(_ sender: UIButton) {
         totalSeconds = eggTimes[sender.currentTitle!]! * 60
-        
+
         timer.invalidate()
         timer = Timer.scheduledTimer(
             timeInterval: 1.0,
@@ -33,10 +33,10 @@ class ViewController: UIViewController {
             repeats: true
         )
     }
-    
+
     @objc func updateTimer() {
         if secondsPassed < totalSeconds {
-            let progress = Float(secondsPassed)/Float(totalSeconds)
+            let progress = Float(secondsPassed) / Float(totalSeconds)
             progressBar.progress = progress
             print(progress)
             secondsPassed += 1
@@ -45,5 +45,5 @@ class ViewController: UIViewController {
             topTitle.text = "Done!"
         }
     }
- 
+
 }
