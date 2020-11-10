1a521a832da628179ee43eb181ba9adddc8ad9b3
Matthew Gleich <email@mattglei.ch>
2020-11-10 00:11:27 -0500

    🎉 Initialize quizzler application

---
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Matthew Gleich on 11/10/2020
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
