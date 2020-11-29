2aa2bf6e9ce9c630232e7b5e1a9d0d3d312b5cb6
Matthew Gleich <email@mattglei.ch>
2020-11-29 00:53:26 -0500

    🌱 Init Tipsy app

---
+++ Tipsy/Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Matthew Gleich on 11/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
