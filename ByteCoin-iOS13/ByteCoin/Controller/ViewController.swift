a58eabc71c60176c6ec29164de98a35881b5c7eb
Matthew Gleich <email@mattglei.ch>
2020-12-26 18:31:12 -0500

    🎉 Init byteCoin app

---
+++ ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Matthew Gleich on 12/26/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,14 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Matthew Gleich on 12/26/2020.
+//
+
+import Foundation
+
+struct CoinManager {
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+
+    let currencyArray = ["AUD", "BRL", "CAD", "CNY", "EUR", "GBP", "HKD", "IDR", "ILS", "INR", "JPY", "MXN", "NOK", "NZD", "PLN", "RON", "RUB", "SEK", "SGD", "USD", "ZAR"]
+}
