5701036f552c8ab19eedf9114133de990e07176d
Matthew Gleich <email@mattglei.ch>
2020-12-26 18:51:32 -0500

    ✨ Implement basic data picker

--- ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
+++ ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
@@ -7,9 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate {
+    @IBOutlet var currencyLabel: UILabel!
+    @IBOutlet var bitCoinLabel: UILabel!
+    @IBOutlet var currencyPicker: UIPickerView!
+
+    let coinManager = CoinManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+    }
+}
+
+// MARK: - UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
     }
 }
